71fc1b151e51e634d71c43a235f62487145bce86
abfalex <87718997+abfalex@users.noreply.github.com>
2024-07-05 21:50:07 +0300

    Add Place and PlaceImage models

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Place, PlaceImage
 
-# Register your models here.
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+
+
+@admin.register(PlaceImage)
+class PlaceImageAdmin(admin.ModelAdmin):
+    list_display = ("__str__",)
--- places/models.py
+++ places/models.py
@@ -1,4 +1,20 @@
 from django.db import models
 
-# Create your models here.
-print("lol")
+
+class Place(models.Model):
+    title = models.CharField("Название", max_length=255, blank=True)
+    description_short = models.TextField("Короткое описание", blank=True)
+    description_long = models.TextField("Длинное описание", blank=True)
+    lat = models.FloatField("Широта", blank=True)
+    lng = models.FloatField("Долгота", blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class PlaceImage(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="images")
+    image = models.ImageField("Изображения", upload_to="place_images")
+
+    def __str__(self):
+        return self.place.title
