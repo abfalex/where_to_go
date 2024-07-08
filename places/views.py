272476f03f82837bf5b8e487df80ad9a50962cc5
abfalex <87718997+abfalex@users.noreply.github.com>
2024-07-08 15:16:22 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,6 +1,28 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.templatetags.static import static
+
+from .models import Place, PlaceImage
 
 
 def home(request):
-    return render(request, "places/index.html")
+    places = Place.objects.all()
+
+    features = []
+    for place in places:
+        features.append(
+            {
+                "type": "Feature",
+                "geometry": {"type": "Point", "coordinates": [place.lng, place.lat]},
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.place_id,
+                    "detailsUrl": static(f"/places/{place.place_id}.json"),
+                },
+            }
+        )
+
+    geojson_data = {"type": "FeatureCollection", "features": features}
+
+    context = {"geojson_data": geojson_data}
+
+    return render(request, "places/index.html", context=context)
