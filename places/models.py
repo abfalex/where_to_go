8d02f56fc38c9237e9c8783e8688c2b76d0a66e2
abfalex <87718997+abfalex@users.noreply.github.com>
2024-07-08 15:14:58 +0300

    add Place.place_id column

--- places/models.py
+++ places/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Place(models.Model):
     title = models.CharField("Название", max_length=255, blank=True)
+    place_id = models.CharField("Айди", max_length=255, blank=True)
     description_short = models.TextField("Короткое описание", blank=True)
     description_long = models.TextField("Длинное описание", blank=True)
     lat = models.FloatField("Широта", blank=True)
