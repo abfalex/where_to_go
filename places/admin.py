e1ce9349b0e162bb27c485d06ca22632d6e2c9ce
abfalex <87718997+abfalex@users.noreply.github.com>
2024-08-01 13:24:09 +0300

    add image field to place field

--- places/admin.py
+++ places/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from .models import Place, PlaceImage
 
 
+class PlaceImageInline(admin.TabularInline):
+    model = PlaceImage
+    extra = 1
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ("title",)
+    inlines = [PlaceImageInline]
 
 
 @admin.register(PlaceImage)
